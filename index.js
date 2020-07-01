2617f8ff43a2457357776dad124ddb6ece929109
zbarovsky <zachary.barovsky@gmail.com>
2020-06-30 17:16:33 -0700

    fixed bugs

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(passport.session());
 app.use(flash());
 
 app.use(function(req, res, next) {
-    res.locals.alert = req.flash();
+    res.locals.alerts = req.flash();
     res.locals.currentUser = req.user;
 
     next();
